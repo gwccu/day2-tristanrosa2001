4a8d6fa8b5f40bb4b198f6037f2f9a7523fe18eb
tristanrosa2001 <trissy.rosa@gmail.com>
2019-02-23 15:36:55 +0000

    Day 3 Warm up

---
+++ day3.py
@@ -0,0 +1,6 @@
+print("My code uses my name as a variable known as my Name where I add my last name and multiply it 78 times")
+print("tristan")
+myName = "tristan"
+print(myName)
+print(myName + "Rosa")
+print((myName + "Rosa")*78)
